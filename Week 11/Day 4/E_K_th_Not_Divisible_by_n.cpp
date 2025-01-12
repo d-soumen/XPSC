52e3d18f48cc7cdb71aba1155722980288eab6d3
d-soumen <sd6825663@gmail.com>
2025-01-12 23:21:55 +0600

    Add files via upload

---
+++ Week 11/Day 4/E_K_th_Not_Divisible_by_n.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define vec vector<int>
+#define vl vector<ll>
+#define fl(i,n) for(int i=0;i<n;i++)
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+using namespace std;
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    
+    auto ok=[&](int mid){
+        return (mid-(mid/n))>=k;
+        };
+
+    int l=1,r=2e9,ans,mid;
+    while(l<=r){
+        mid=l+(r-l)/2;
+        if(ok(mid)){
+            ans=mid;
+            r=mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+    }
+    cout<<ans<<endl;
+}
+int main(){
+    fast_io
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
